31cc762a314e1803720195a1781059e3bd59aa95
Ektamadaan <ektamadaan45@gmail.com>
2023-12-28 21:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const bname = navigator.appName;
+const bversion = navigator.appVersion;
+
+const msg = "You are using" + bname + "version" + bversion ;
+let input1 = document.getElementById("browser-info");
+input1.textContent = msg;
